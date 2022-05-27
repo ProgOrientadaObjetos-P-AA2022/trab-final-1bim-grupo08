8c7a9e89b3da6b5595e8c1a76023d9d334e02017
ivanooo9 <marcoeli098@gmail.com>
2022-05-27 12:23:44 -0500

    Cambios paquete 6

--- AdministrarInmobiliaria/src/paquete06/Casa.java
+++ AdministrarInmobiliaria/src/paquete06/Casa.java
@@ -16,4 +16,114 @@ import paquete05.Constructora;
  */
 public class Casa {
 
+    private Propietario prop;
+    private double precioMetroCuadrado;
+    private double metrosCuadrados;
+    private double costoFinal;
+    private Barrio ba;
+    private Ciudad ci;
+    private int numeroCuartos;
+    private Constructora cons;
+
+    public Casa() {
+
+    }
+
+    public Casa(Propietario p, double pM, double mC, Barrio b, Ciudad c, int nC, Constructora co) {
+        prop = p;
+        precioMetroCuadrado = pM;
+        metrosCuadrados = mC;
+        ba = b;
+        ci = c;
+        numeroCuartos = nC;
+        cons = co;
+    }
+
+    public void establecerPropietario(Propietario c) {
+        prop = c;
+    }
+
+    public Propietario obtenerPropietario() {
+        return prop;
+    }
+
+    public void establecerPrecioMetroCuadrado(double c) {
+        precioMetroCuadrado = c;
+    }
+
+    public double obtenerPrecioMetroCuadrado() {
+        return precioMetroCuadrado;
+    }
+
+    public void establecerMetrosCuadrados(double c) {
+        metrosCuadrados = c;
+    }
+
+    public double obtenerMetrosCuadrados() {
+        return metrosCuadrados;
+    }
+
+    public void establecerBarrio(Barrio c) {
+        ba = c;
+    }
+
+    public Barrio obtenerBarrio() {
+        return ba;
+    }
+
+    public void establecerCiudad(Ciudad c) {
+        ci = c;
+    }
+
+    public Ciudad obtenerCiudad() {
+        return ci;
+    }
+
+    public void establecerNumerodeCuartos(int c) {
+        numeroCuartos = c;
+    }
+
+    public int obtenerNumerodeCuartos() {
+        return numeroCuartos;
+    }
+
+    public void establecerConstructora(Constructora c) {
+        cons = c;
+    }
+
+    public Constructora obtenerConstructora() {
+        return cons;
+    }
+
+    public void calcularCostoFinal() {
+        costoFinal = precioMetroCuadrado * metrosCuadrados;
+    }
+
+    public double obtenerCostoFinal() {
+        return costoFinal;
+    }
+
+    @Override
+    public String toString() {
+        String c = "Información Casa";
+        c = String.format("%s\n "
+                + "%s\n  "
+                + "Precio Metro Cuadrado: %.2f\n "
+                + "Numero de Metros Cuadrados: %.2f\n"
+                + "Costo Final: %.2f\n "
+                + "%s\n"
+                + " %s\n "
+                + "Numero de Cuartos: %d\n "
+                + "%s\n", c,
+                obtenerPropietario(),
+                obtenerPrecioMetroCuadrado(),
+                obtenerMetrosCuadrados(),
+                obtenerCostoFinal(),
+                obtenerBarrio(),
+                obtenerCiudad(),
+                obtenerNumerodeCuartos(),
+                obtenerConstructora());
+        return c;
+
+    }
    }
